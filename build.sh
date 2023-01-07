4b0ab021d28b4c00837997732dc94643770938e9
deepikavats <deepikavats12@gmail.com>
2023-01-07 21:10:10 +0100

    Added bash script

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
 mvn package
+
+result=$(curl -X GET --header "Accept: */*" "https://8081-cs-73f3bf7e-48b8-4f95-a6b5-7f6de9ac6684.cs-europe-west1-onse.cloudshell.dev/getLicense?compilerName=gcc")
+echo "Response from server"
+echo $result
