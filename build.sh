f1b45ddf263a48dd2df32f772fd85c79bfea3074
deepikavats <deepikavats12@gmail.com>
2023-01-08 11:50:58 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 mvn package
 
-result=$(curl -X GET --header "Accept: */*" "https://8081-cs-73f3bf7e-48b8-4f95-a6b5-7f6de9ac6684.cs-europe-west1-onse.cloudshell.dev/getLicense?compilerName=gcc")
+result=$(curl -X GET --header "Accept: */*" "https://35.232.115.31:8081/getLicense?compilerName=gcc")
 echo "Response from server"
 echo $result
