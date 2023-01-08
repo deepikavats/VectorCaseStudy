06f96f73c9d13d357666d18ae8d4b8cb871f2a4e
deepikavats <deepikavats12@gmail.com>
2023-01-08 11:53:55 +0100

    Changed https to http

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 mvn package
 
-result=$(curl -X GET --header "Accept: */*" "https://35.232.115.31:8081/getLicense?compilerName=gcc")
+result=$(curl -X GET --header "Accept: */*" "http://35.232.115.31:8081/getLicense?compilerName=gcc")
 echo "Response from server"
 echo $result
