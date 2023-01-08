a795a1117d3dac30b7f2e687e5ca573d77d63676
deepikavats <deepikavats12@gmail.com>
2023-01-08 20:31:08 +0100

    Added Global URL in shell script

--- callapi.sh
+++ callapi.sh
@@ -1,12 +1,12 @@
-result=$(curl -X GET --header "Accept: */*" "http://localhost:8081/reserveLicense?compilerName=javac")
+result=$(curl -X GET --header "Accept: */*" "http://35.232.115.31:8081/reserveLicense?compilerName=javac")
 echo "Response from server"
 echo $result
 if [ $result == "Successful" ]
 then
-  maven package
-  curl -X GET --header "Accept: */*" "http://localhost:8081/freeLicense"
+  mvn package
+  curl -X GET --header "Accept: */*" "http://35.232.115.31:8081/freeLicense"
 else
   echo "License is not available"
-  exit -1
+  exit 1
 fi
 exit
